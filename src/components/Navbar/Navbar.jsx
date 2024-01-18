02090cdc3fd351fff9c455bdb355d2b009e828e1
Ellie <eagyeman1234@gmail.com>
2024-01-18 15:01:24 -0500

    fix import logo error

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
 
+import mainLogo from '../images/logo1.png'
 
 const Navbar = () => {
   return ( 
     <div>
-      Navbar
+      {/* Left */}
+      <div className=''>
+        <img src={mainLogo} 
+        alt='logo'
+        className='w-40'/>
+      </div>
+      {/* Middle */}
+      {/* Right */}
+
     </div>
-)
+  )
 }
 
 export default Navbar
