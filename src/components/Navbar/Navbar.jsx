32bd716a6ba077f1465e477b64e9faaed3a9d2c6
Ellie <eagyeman1234@gmail.com>
2024-01-18 17:30:49 -0500

    add css to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { IoMdMenu } from 'react-icons/io'
 import { FaRegUser } from 'react-icons/fa'
 const Navbar = () => {
   return ( 
-    <div className='flex items-center border-b'>
+    <div className='flex justify-between items-center border-b bg-yellow-200'>
       {/* Left */}
       <div className='h-20 flex'>
         <img src={mainLogo} alt='main-logo'className='object-cover -my-10'/>
@@ -20,10 +20,10 @@ const Navbar = () => {
         </div>
       </div>
       {/* Right */}
-      <div>
+      <div className='flex items-center pr-3'>
         <p>Airbnb Your Home</p>
         <GiWorld />
-        <div className=''>
+        <div className='flex items-center'>
         <IoMdMenu />
         <FaRegUser />
         </div>
