eba70a29c83b58bd8182c749bf9889f7be28d000
Ellie <eagyeman1234@gmail.com>
2024-01-18 17:26:22 -0500

    install react icons and import icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,33 @@
 
 import mainLogo from '../images/logo1.png'
-
+import { GiWorld } from 'react-icons/gi'
+import { IoMdMenu } from 'react-icons/io'
+import { FaRegUser } from 'react-icons/fa'
 const Navbar = () => {
   return ( 
-    <div>
+    <div className='flex items-center border-b'>
       {/* Left */}
-      <div className='bg-red-300 w-40 '>
-        <img src={mainLogo} 
-        alt='main-logo'
-        className=''/>
+      <div className='h-20 flex'>
+        <img src={mainLogo} alt='main-logo'className='object-cover -my-10'/>
       </div>
       {/* Middle */}
+      <div className='flex'>
+        <input type='search' placeholder=''/>
+        <div className=''>
+          <button>Place</button>
+          <button>Time</button>
+          <button>Group Size</button>
+        </div>
+      </div>
       {/* Right */}
+      <div>
+        <p>Airbnb Your Home</p>
+        <GiWorld />
+        <div className=''>
+        <IoMdMenu />
+        <FaRegUser />
+        </div>
+      </div>
 
     </div>
   )
