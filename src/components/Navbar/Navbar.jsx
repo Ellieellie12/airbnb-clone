843a00cb25b9da4dc023032830359c228cdf9cd2
Ellie <eagyeman1234@gmail.com>
2024-01-19 03:37:53 -0500

    import search icon for navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import mainLogo from '../images/logo1.png'
 import { GiWorld } from 'react-icons/gi'
 import { IoMdMenu } from 'react-icons/io'
 import { FaRegUser } from 'react-icons/fa'
+import { CiSearch } from "react-icons/ci";
 const Navbar = () => {
   return ( 
     <div className='flex justify-between items-center border-b'>
@@ -11,12 +12,15 @@ const Navbar = () => {
         <img src={mainLogo} alt='main-logo'className='object-cover -my-10'/>
       </div>
       {/* Middle */}
-      <div className='flex relative shadow-sm shadow-gray-400 rounded-full'>
-        <input type='search' placeholder='' className='py-2.5 w-[20rem] rounded-full'/>
+      <div className='flex justify-center relative shadow-sm shadow-gray-400 rounded-full'>
+        <input type='search' placeholder='' className='py-2.5 w-[20rem] rounded-full outline-0'/>
         <div className='absolute'>
           <button>Place</button>
           <button>Time</button>
           <button>Group Size</button>
+        </div>
+        <div className=''>
+          <CiSearch />
         </div>
       </div>
       {/* Right */}
