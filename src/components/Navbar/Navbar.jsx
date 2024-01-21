79dcf9bb0b3caa450c31c8df8466671100f5b60e
Ellie <eagyeman1234@gmail.com>
2024-01-21 15:54:13 -0500

    add css to sign in

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,41 +4,43 @@ import { GiWorld } from 'react-icons/gi'
 // import { IoMdMenu } from 'react-icons/io'
 import { FaRegUser } from 'react-icons/fa'
 import { CiSearch } from 'react-icons/ci'
-import { FaHouseChimney } from 'react-icons/fa6'
+// import { FaHouseChimney } from 'react-icons/fa6'
 
 const Navbar = () => {
   return ( 
-    <div className='flex justify-between items-center border-b'>
-      {/* Left */}
-      <div className='h-20 flex'>
-        <img src={mainLogo} alt='main-logo'className='object-cover -my-10'/>
-      </div>
-      {/* Middle */}
-      <div className='hidden lg:flex justify-center items-center relative shadow-sm shadow-gray-400 rounded-full'>
-        <input type='search' placeholder='' className='py-2.5 w-[20rem] rounded-full outline-0'/>
-        <div className='flex justify-between absolute w-full pr-16 pl-6 font-semibold text-gray-600'>
-          <button className='w-full'>Place</button>
-          <button className='border-l border-x px-6'>Time</button>
-          <button className='w-full text-gray-600/60 pl-2'>Group Size</button>
+    <div className='border-b'> 
+      <div className='flex justify-between items-center sm:mx-6 md:mx-10 lg:mx-12'>
+        {/* Left */}
+        <div className='h-20 flex'>
+          <img src={mainLogo} alt='main-logo'className='object-cover -my-10'/>
         </div>
-        <div className='bg-[#ff5a60] p-2 rounded-full mr-2'>
-          <CiSearch className='text-white'/>
+        {/* Middle */}
+        <div className='hidden lg:flex justify-center items-center relative shadow-sm shadow-gray-400 rounded-full'>
+          <input type='search' placeholder='' className='py-2.5 w-[20rem] rounded-full outline-0'/>
+          <div className='flex justify-between absolute w-full pr-16 pl-6 font-semibold text-gray-600'>
+            <button className='w-full'>Place</button>
+            <button className='border-l border-x px-6'>Time</button>
+            <button className='w-full text-gray-600/60 pl-2'>Group Size</button>
+          </div>
+          <div className='bg-[#ff5a60] p-2 rounded-full mr-2'>
+            <CiSearch className='text-white'/>
+          </div>
         </div>
-      </div>
-      {/* Right */}
-      <div className='flex items-center pr-3 font-semibold text-gray-600'>
-          <p className='text-[17px]'>Rent House</p>
-        
+        {/* Right */}
+        <div className='flex items-center pr-3 font-semibold text-gray-600'>
+            <p className='text-[17px]'>Rent House</p>
+          
 
-        <div className='flex items-center mx-8 gap-1'>
-          <GiWorld className=''/>
-          <div className=''>EN</div>
-        </div>
+          <div className='flex items-center mx-8 gap-1'>
+            <GiWorld className=''/>
+            <div className=''>EN</div>
+          </div>
 
-        <div className='flex items-center border px-3 py-2 rounded-full gap-2 shadow-lg shadow-gray-300 hover:bg-[#ee7c80] duration-100 ease-out'>
-          <p>Sign In</p>
-          {/* <IoMdMenu className='text-[19px]' /> */}
-          <FaRegUser className='text-[19px]'/>
+          <div className='flex items-center border px-3 py-2 rounded-full gap-2 shadow-lg bg-[#ff5a60] shadow-gray-300 hover:bg-[#ea6e72] duration-100 ease-out'>
+            <p>Sign In</p>
+            {/* <IoMdMenu className='text-[19px]' /> */}
+            <FaRegUser className='text-[19px]'/>
+          </div>
         </div>
       </div>
     </div>
