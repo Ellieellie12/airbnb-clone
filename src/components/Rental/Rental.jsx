9e2352f07b8f89d71a93e6db317ea64fa54dfab9
Ellie <eagyeman1234@gmail.com>
2024-01-25 15:43:14 -0500

    import Rental to Rentals component

---
+++ src/components/Rental/Rental.jsx
@@ -0,0 +1,10 @@
+
+
+const Rental = () => {
+  return (
+    <div>Rental</div>
+  )
+}
+
+export default Rental
+
--- src/components/Rentals/Rentals.jsx
+++ src/components/Rentals/Rentals.jsx
@@ -1,10 +1,23 @@
 import houseone from '../images/house1.jpg'
+import Rental from '../Rental/Rental'
 
 
 const Rentals = () => {
-  const rentals = [{title:'Texas, USA', image: houseone}]
+  const rentals = [{title:'Texas, USA', image: houseone, price:'1,541'}]
+  const rentals = [{title:'Texas, USA', image: houseone, price:'1,541'}]
+  const rentals = [{title:'Texas, USA', image: houseone, price:'1,541'}]
+  const rentals = [{title:'Texas, USA', image: houseone, price:'1,541'}]
+  const rentals = [{title:'Texas, USA', image: houseone, price:'1,541'}]
+  const rentals = [{title:'Texas, USA', image: houseone, price:'1,541'}]
+  const rentals = [{title:'Texas, USA', image: houseone, price:'1,541'}]
+  const rentals = [{title:'Texas, USA', image: houseone, price:'1,541'}]
+  const rentals = [{title:'Texas, USA', image: houseone, price:'1,541'}]
+  const rentals = [{title:'Texas, USA', image: houseone, price:'1,541'}]
+
   return (
-    <div className='pt-3'>Rentals</div>
+    <div className='pt-3'>
+      {rentals.map((rental) => <Rental />)}
+    </div>
   )
 }
 
