5ff68ea13e5087da68629411e11cd0088edc9217
Ellie <eagyeman1234@gmail.com>
2024-01-26 13:25:13 -0500

    fix gradient error

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,6 @@
 const Filter = ({icon, title}) => {
   return(
-    <div className='flex items-center bg-[#ff5a60] text-white hover:bg-white hover:text-[#ff5a60]  duration-200 ease-out gap-2 py-1 px-3 sm:px-4 rounded-full text-[15px] sm:text-[16px]'>
+    <div className='flex items-center bg-[#ff5a60] text-white hover:bg-white hover:text-[#ff5a60]  duration-200 ease-out gap-2 py-1 px-3 sm:px-4 rounded-full text-[14px] sm:text-[16px]'>
       {icon}
       {title}
     </div>
--- src/components/Rental/Rental.jsx
+++ src/components/Rental/Rental.jsx
@@ -2,9 +2,12 @@
 
 const Rental = ({title, image, price}) => {
   return (
-    <div className=''>
-      <img src={image} alt="" />
-
+    <div className='relative'>
+      <div className='grad absolute h-full w-full'></div>
+      <div className='flex '>
+        <img src={image} alt="" className='object-cover rounded-[1.3rem]' />
+        <div className='absolute text-white font-bold bottom-0'>{title}</div>
+      </div>
     </div>
   )
 }
