a45ddc173ea7db0677fc005eb52d58fd98aae0b4
Ellie <eagyeman1234@gmail.com>
2024-01-29 10:37:54 -0500

    add description for homes

--- src/components/Rental/Rental.jsx
+++ src/components/Rental/Rental.jsx
@@ -2,14 +2,25 @@
 
 const Rental = ({title, image, price}) => {
   return (
-    <div className='relative'>
-      <div className='grad absolute h-full w-full rounded-b-[1.3rem]'></div>
+    <div className=''>
+      <div className='relative'>
+        <div className='grad absolute h-full w-full rounded-b-[1.3rem]'></div>
       <div className='flex '>
+        {/* Background Image  */}
         <img src={image} alt="" className='object-cover rounded-[1.3rem]' />
-        <div className='absolute text-white font-bold bottom-6 left-6 text-[22px] flex items-center gap-4'>
+        {/* Title */}
+        <div className='absolute text-white font-bold bottom-6 left-6 text-[22px] flex items-center gap-2'>
           {title}
-          <p className='text-[18px]'>${price}</p>
-          </div>
+          <span> &#x2022;</span>
+          <p className='text-[18px] text-slate-200'>${price}</p>
+        </div>
+        </div>
+      </div>
+      {/* Description */}
+      <div className=''> 
+        <p className='max-w-[17rem]'>
+        This is a rare find. It is usually fully booked 
+        </p>
       </div>
     </div>
   )
