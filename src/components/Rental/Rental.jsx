6697e378f6cab26034a0f0affe7ee0a12f54165f
Ellie <eagyeman1234@gmail.com>
2024-01-29 08:48:07 -0500

    add price to photo

--- src/components/Rental/Rental.jsx
+++ src/components/Rental/Rental.jsx
@@ -6,7 +6,10 @@ const Rental = ({title, image, price}) => {
       <div className='grad absolute h-full w-full rounded-b-[1.3rem]'></div>
       <div className='flex '>
         <img src={image} alt="" className='object-cover rounded-[1.3rem]' />
-        <div className='absolute text-white font-bold bottom-0'>{title}</div>
+        <div className='absolute text-white font-bold bottom-6 left-6 text-[22px] flex items-center gap-4'>
+          {title}
+          <p className='text-[18px]'>${price}</p>
+          </div>
       </div>
     </div>
   )
