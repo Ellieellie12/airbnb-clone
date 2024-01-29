29829844c19783a94f893597f72241f340e2fbd2
Ellie <eagyeman1234@gmail.com>
2024-01-29 08:39:45 -0500

    add rounded bottom to pictures

--- src/components/Rental/Rental.jsx
+++ src/components/Rental/Rental.jsx
@@ -3,7 +3,7 @@
 const Rental = ({title, image, price}) => {
   return (
     <div className='relative'>
-      <div className='grad absolute h-full w-full'></div>
+      <div className='grad absolute h-full w-full rounded-b-[1.3rem]'></div>
       <div className='flex '>
         <img src={image} alt="" className='object-cover rounded-[1.3rem]' />
         <div className='absolute text-white font-bold bottom-0'>{title}</div>
