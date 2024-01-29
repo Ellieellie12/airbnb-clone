b9fca42eaa021b66788d7d442074f0df211f9359
Ellie <eagyeman1234@gmail.com>
2024-01-29 15:50:52 -0500

    add columns for mobile sizes med lg and xl

--- src/components/Rentals/Rentals.jsx
+++ src/components/Rentals/Rentals.jsx
@@ -20,7 +20,7 @@ const Rentals = () => {
 
   return (
     <div className='pt-3'>
-      <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4'>
         {rentals.map((rental) => 
         <Rental 
           title={rental.title} 
