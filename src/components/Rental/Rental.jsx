f9c6e657c6dab8014c378a2def04a7a423c0bc62
Ellie <eagyeman1234@gmail.com>
2024-01-30 18:37:07 -0500

    fix mobile view

--- src/components/Rental/Rental.jsx
+++ src/components/Rental/Rental.jsx
@@ -8,7 +8,7 @@ const Rental = ({title, image, price}) => {
         <div className='grad absolute h-full w-full rounded-b-[1.3rem]'></div>
       <div className='flex '>
         {/* Background Image  */}
-        <img src={image} alt="" className='object-cover rounded-[1.3rem] max-h-[14.5rem]' />
+        <img src={image} alt="" className='object-cover rounded-[1.3rem] sm:max-h-[14.5rem]' />
         {/* Title */}
         <div className='absolute text-white font-bold bottom-6 left-6 text-[22px] flex items-center gap-2'>
           {title}
