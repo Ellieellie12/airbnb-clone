90b35455f08a7f4c62724af950b61afc5a89e157
Ellie <eagyeman1234@gmail.com>
2024-02-05 16:09:39 -0500

    fix overlapping of images to navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,7 @@
 
 const Footer = () => {
   return ( 
-    <div className='relative'>
-      <div className='h-20 bg-white absolute'>Footer</div>
-    </div>
+      <div className='h-20 w-full bg-red-400 sticky bottom-0'>Footer</div>
     )
 }
 export default Footer
