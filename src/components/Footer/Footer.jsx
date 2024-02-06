df8751a8b1eee9ba0e11d0631ee134bd44c171dd
Ellie <eagyeman1234@gmail.com>
2024-02-06 14:31:48 -0500

    import snapchat social icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,20 @@
 import { FaXTwitter } from 'react-icons/fa6'
 import { FaInstagram } from 'react-icons/fa'
-import { FaFacebookSquare } from react-icons/fa'
+import { FaFacebookSquare } from 'react-icons/fa'
+import { FaSnapchat } from 'react-icons/fa6'
 
 const Footer = () => {
+  const socialicons = [
+  <FaXTwitter />, 
+  <FaInstagram />, 
+  <FaFacebookSquare />,
+  <FaSnapchat />
+]
   return ( 
       <div className='flex items-center justify-center h-20 w-full bg-white border-t-2 shadow-t-lg shadow-gray-300 sticky bottom-0'>
-        <p className='font-bold text-[19px]'>Social Media</p>
+        {socialicons.map((socialicon) => (
+          <div className=''></div>
+        ))}
       </div>
     )
 }
