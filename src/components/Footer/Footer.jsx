8bcb05ec80a13670e8c05e8b65fe1da659a31e56
Ellie <eagyeman1234@gmail.com>
2024-02-06 14:27:17 -0500

    import social media icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
-
+import { FaXTwitter } from 'react-icons/fa6'
+import { FaInstagram } from 'react-icons/fa'
+import { FaFacebookSquare } from react-icons/fa'
 
 const Footer = () => {
   return ( 
-      <div className='h-20 w-full bg-white border-t-2 shadow-t-lg shadow-gray-300 sticky bottom-0'>
-        <p>Footer</p>
+      <div className='flex items-center justify-center h-20 w-full bg-white border-t-2 shadow-t-lg shadow-gray-300 sticky bottom-0'>
+        <p className='font-bold text-[19px]'>Social Media</p>
       </div>
     )
 }
 export default Footer
+
+<FaXTwitter />
+<FaInstagram />
+<FaFacebookSquare />
